42697929cdb4cfb0c4607d7e409ecf6779aa378d
murilonicemento <murilo_nascimento22@outlook.com>
2025-04-13 17:17:11 -0300

    feat: create elastic search model class configuration

---
+++ OCRSearch.Infrastructure/Configurations/ElasticSearchConfiguration.cs
@@ -0,0 +1,9 @@
+﻿namespace OCRSearch.Infrastructure.Configurations;
+
+public class ElasticSearchConfiguration
+{
+    public string Url { get; set; }
+    public string DefaultIndex { get; set; }
+    public string Username { get; set; }
+    public string Password { get; set; }
+}
