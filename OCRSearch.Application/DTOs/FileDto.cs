19d19d33a02182be289ee06b3981eda76fc36159
murilonicemento <murilo_nascimento22@outlook.com>
2025-04-16 16:41:36 -0300

    feat: create file dto

---
+++ OCRSearch.Application/DTOs/FileDto.cs
@@ -0,0 +1,14 @@
+﻿namespace OCRSearch.Application.DTOs;
+
+public class FileDto
+{
+    public string Id { get; set; }
+
+    public string Name { get; set; }
+
+    public string Url { get; set; }
+
+    public string ExtractedText { get; set; }
+
+    public DateTime UploadedAt { get; set; }
+}
