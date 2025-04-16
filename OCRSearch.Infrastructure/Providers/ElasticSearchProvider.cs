2b18d6f8f44e4fb5d15c06c9ffede46f36c93c05
murilonicemento <murilo_nascimento22@outlook.com>
2025-04-16 16:40:54 -0300

    feat: implement index document method

--- OCRSearch.Infrastructure/Providers/ElasticSearchProvider.cs
+++ OCRSearch.Infrastructure/Providers/ElasticSearchProvider.cs
@@ -1,12 +1,46 @@
-﻿using OCRSearch.Application.Interfaces;
+﻿using Elastic.Clients.Elasticsearch;
+using Microsoft.Extensions.Options;
+using OCRSearch.Application.DTOs;
+using OCRSearch.Application.Interfaces;
+using OCRSearch.Infrastructure.Configurations;
+using OCRSearch.Infrastructure.Models;
 
 namespace OCRSearch.Infrastructure.Providers;
 
 public class ElasticSearchProvider : ISearchProvider
 {
-    public Task IndexDocumentAsync()
+    private readonly ElasticsearchClient _client;
+
+    public ElasticSearchProvider(IOptions<ElasticSearchConfiguration> options)
     {
-        throw new NotImplementedException();
+        var configuration = options.Value;
+        var settings =
+            new ElasticsearchClientSettings(new Uri(configuration.Url)).DefaultIndex(configuration.DefaultIndex);
+        _client = new ElasticsearchClient(settings);
+    }
+
+    public async Task<bool> IndexDocumentAsync(FileDto fileDto, string indexName)
+    {
+        var indexExist = (await _client.Indices.ExistsAsync(indexName)).Exists;
+
+        if (!indexExist)
+        {
+            await _client.Indices.CreateAsync(indexName);
+        }
+
+        var file = new FileModel
+        {
+            Id = fileDto.Id,
+            Name = fileDto.Name,
+            ExtractedText = fileDto.ExtractedText,
+            Url = fileDto.Url,
+            UploadedAt = fileDto.UploadedAt
+        };
+
+        var response = await _client.IndexAsync(file,
+            i => i.Index(indexName).OpType(OpType.Index));
+
+        return response.IsValidResponse;
     }
 
     public Task SearchDocumentAsync()
