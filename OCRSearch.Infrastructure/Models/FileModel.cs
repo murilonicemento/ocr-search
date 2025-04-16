bd142c3de00625bc7c9bf61850ab984cf52bc56b
murilonicemento <murilo_nascimento22@outlook.com>
2025-04-16 16:41:49 -0300

    feat: create file model

---
+++ OCRSearch.Infrastructure/Models/FileModel.cs
@@ -0,0 +1,14 @@
+﻿namespace OCRSearch.Infrastructure.Models;
+
+public class FileModel
+{
+    public string Id { get; set; }
+
+    public string Name { get; set; }
+
+    public string Url { get; set; }
+
+    public string ExtractedText { get; set; }
+
+    public DateTime UploadedAt { get; set; }
+}
