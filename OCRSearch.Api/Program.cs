f333dbb7ad901959fa67dbe84c503033b9f4d4d1
murilonicemento <murilo_nascimento22@outlook.com>
2025-04-26 18:13:08 -0300

    feat: add els configuration service

--- OCRSearch.Api/Program.cs
+++ OCRSearch.Api/Program.cs
@@ -1,6 +1,7 @@
 using OCRSearch.Application.Interfaces;
 using OCRSearch.Application.Services;
 using OCRSearch.Infrastructure.Configurations;
+using OCRSearch.Infrastructure.Providers;
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,7 +12,10 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.Configure<ElasticSearchConfiguration>(builder.Configuration.GetSection("ElasticSearchConfiguration"));
+
 builder.Services.AddScoped<IFileService, FileService>();
+builder.Services.AddScoped<ISearchProvider, ElasticSearchProvider>();
 
 var app = builder.Build();
 
